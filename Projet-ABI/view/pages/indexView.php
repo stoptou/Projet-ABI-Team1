1c2a337c23446dbdb00e17abc2bba67cde6f4776
Marine <m.venon45@gmail.com>
2022-04-14 17:39:56 +0200

    1

--- Projet-ABI/view/pages/indexView.php
+++ Projet-ABI/view/pages/indexView.php
@@ -1,4 +1,4 @@
-<?php $title='Acceuil'; ?>
+<?php $title='Accueil'; ?>
 <?php ob_start(); ?>
 
 <div class="px-0 mx-0 d-none d-md-block banniere">
@@ -27,7 +27,7 @@
                 
                 <div class="row rejoindre px-0 mx-0">
                     <div class="col-12 col-md-6 mx-0 px-0">
-                        <img class="" alt="Image à trouver" src="./public/IMG/" width="100%">
+                        <img class="" alt="Image à trouver" src="./public/IMG/accueil1.jpeg" width="100%">
                     </div>
                     <div class="col-12 col-md-6 text-center p-4">
                         <h3 class="titre-rejoindre">Titre présentation 2 à créer</h3>
