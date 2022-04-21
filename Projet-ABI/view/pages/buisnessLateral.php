6c86a02d24e5314585e9b39a65a33937f0fb4e5f
Marine <m.venon45@gmail.com>
2022-04-21 17:04:00 +0200

    Mise en place des SVG
    Dans la partie gestion Commerciale, insertion des svg

--- Projet-ABI/view/pages/buisnessLateral.php
+++ Projet-ABI/view/pages/buisnessLateral.php
@@ -1,16 +1,16 @@
 
     <div class="col-2 lateral navbar text-center px-4 ml-4 mr-0">
         <ul class="navbar-nav w-100 h-100">
-            <li class="navbar-item mb-4 picture"><a class="navbar-link" href="./index.php?action=buisness&amp;action2=clientList"><img src="./public/IMG/" alt="image liste client à créer></a></li>
+            <li class="navbar-item mb-4 picture"><a class="navbar-link" href="./index.php?action=buisness&amp;action2=clientList"><img src="./public/IMG/svg/svg/users-between-lines.svg" alt="image liste client à créer"></a></li>
             <li class="navbar-item"><a class="navbar-link invisible" href="#">Lister les clients</a></li>
             <li><hr class="py-2 my-0"></li>  
-            <li class="navbar-item mb-4 picture"><a class="navbar-link" href="./index.php?action=buisness&amp;action2=searchClient"><img src="./public/IMG/" alt="image recherche client à créer></a></li>
+            <li class="navbar-item mb-4 picture"><a class="navbar-link" href="./index.php?action=buisness&amp;action2=searchClient"><img src="./public/IMG/svg/svg/magnifying-glass.svg" alt="image recherche client à créer"></a></li>
             <li class="navbar-item"><a class="navbar-link invisible" href="#">Rechercher des clients</a></li>
             <li><hr class="py-2 my-0"></li> 
-            <li class="navbar-item mb-4 picture"><a class="navbar-link" href="./index.php?action=buisness&amp;action2=addClient"><img src="./public/IMG/" alt="image ajouter client à créer></a></li>
+            <li class="navbar-item mb-4 picture"><a class="navbar-link" href="./index.php?action=buisness&amp;action2=addClient"><img src="./public/IMG/svg/svg/user-plus.svg" alt="image ajouter client à créer"></a></li>
             <li class="navbar-item"><a class="navbar-link invisible" href="#">Ajouter un client</a></li>
             <li><hr class="py-2 my-0"></li>  
-            <li class="navbar-item mb-4 picture"><a class="navbar-link" href="./index.php?action=buisness&amp;action2=delClient"><img src="./public/IMG/" alt="image supprimer client à créer></a></li>
+            <li class="navbar-item mb-4 picture"><a class="navbar-link" href="./index.php?action=buisness&amp;action2=delClient"><img src="./public/IMG/svg/svg/user-minus.svg" alt="image supprimer client à créer"></a></li>
             <li class="navbar-item"><a class="navbar-link invisible" href="#">Supprimer un client</a></li>
             <li><hr class="py-2 my-0"></li>                   
         </ul>
