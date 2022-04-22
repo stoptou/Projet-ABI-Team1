7ff24302a2e12257ec4f45a99c785ce88df530e7
Eric_Caujolle <ericcaujollebert@gmail.com>
2022-04-22 10:04:43 +0200

    implementé formulaire modification données client
    - et suppression client

---
+++ Projet-ABI/view/pages/delClientView.php
@@ -0,0 +1,47 @@
+<?php
+
+use ABI\controller\Buisness;
+
+
+$results=Buisness::viewClients();
+    if(!empty($results))
+                {             
+?>
+
+<div class='row'>
+
+    <div class="col px-4">
+    
+            <table class="table table-hover">
+            <thead>
+            <tr>
+                <th>Raison sociale</th>
+                <th>Adresse</th>
+                <th>Code postal</th>
+                <th>Ville</th>
+                <th></th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+                
+                    foreach($results as $result)
+                    {                      
+            ?>
+            <tr>
+                
+                <td><?= $result['RAISONSOCIALE']?></td>
+                <td><?= $result['ADRESSECLIENT']?></td>
+                <td><?= $result['CODEPOSTALCLIENT']?></td>
+                <td><?= $result['VILLECLIENT']?></td>
+                <td> <a href="../index.php?action=buisness&action2=delClient&IDCLIENT=<?= $result['IDCLIENT']?>">Supprimer</a></td>              
+            </tr>
+            <?php
+                    }               
+            ?>
+            </tbody>
+        </table>
+    </div>
+</div>
+<?php
+    }
